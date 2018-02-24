e033442e5a51df983b5ba1188474f4c9373f389c
Robert Adams <misterblue@misterblue.com>
2018-02-23 19:14:13 -0800

    Remove some intermediate version code -- is is not being developed and is getting very stale.

--- Basiljs-nodejs/jslibs/threejs-wrapper.js
+++
@@ -1,7 +0,0 @@
-// Kludge that seems to allow RequireJS shim exports to work for ThreeJS
-// ref: https://github.com/mrdoob/three.js/issues/9602
-
-define(['real-threejs'], function (THREE) {
-    window.THREE = THREE;
-    return THREE;
-});
