2a5509ccabfbe593c03c8a23028a91625dc18ed9
Robert Adams <misterblue@misterblue.com>
2018-04-16 14:23:44 -0700

    Merge Displayable and DisplayableInstance into one module file.

--- Basiljs/src/Items/Displayable.js
+++ Basiljs/src/Items/Displayable.js
@@ -14,7 +14,89 @@
 import { BItem } from 'xBItem.js';
 
 export class Displayable extends BItem {
-    constructor(id, auth, assetInfo) {
+    constructor(auth, assetInfo) {
+        let id = CreateUniqueId('displayable');
         super(id, auth);
+    }
+}
+
+export class DisplayableInstance extends BItem {
+    constructor(id, auth, baseDisplayable) {
+        super(id, auth);
+        this.displayable = baseDisplayable;
+
+        this.gPos = [ 0, 0, 0 ];
+        this.gRot = [ 0, 0, 0, 1];
+        this.gPosCoordSystem = 0;
+        this.gRotCoordSystem = 0;
+        this.gRotgPosModified = false;  // flag saying gRot or gPos modified
+
+        this.referenceFrame = 0;
+        this.localPos = [ 0,0,0 ];
+        this.localRot = [ 0, 0, 0, 1];
+
+        super.DefineProperties( {
+            'Position': {
+                'get': () => { return this.gPos; },
+                'set': (val) => {
+                    if (typeof val == 'String') {
+                      val = JSON.Parse(val);
+                    }
+                    if (typeof val == 'Array' && val.length == 3) {
+                        this.gPos[0] = Float(val[0]);
+                        this.gPos[1] = Float(val[1]);
+                        this.gPos[2] = Float(val[2]);
+                    }
+                    this.gRotgPosModified = true;
+                    if (this.procgPostionSet !== undefined)
+                        procgPositionSet(this);
+                    }
+                }
+            },
+            'Rotation': {
+                'get': () => { return this.gRot; },
+                'set': (val) => {
+                    if (typeof val == 'Array' && val.length == 4) {
+                        this.gRot[0] = Float(val[0]);
+                        this.gRot[1] = Float(val[1]);
+                        this.gRot[2] = Float(val[2]);
+                        this.gRot[4] = Float(val[4]);
+                    }
+                    this.gRotgPosModified = true;
+                    if (this.procgRotationSet !== undefined)
+                        procgRotationSet(this);
+                    }
+                }
+            },
+            'PosCoordSystem': {
+                'get': () => { return this.gPosCoordSystem; },
+                'set': (val) => {
+                    this.gPosCoordSystem = Integer(val);
+                }
+            },
+            'RotCoordSystem': {
+                'get': () => { return this.gRotCoordSystem; },
+                'set': (val) => {
+                    this.gRotCoordSystem = Integer(val);
+                }
+            },
+            'referenceFrame': {
+                'get': () => { return this.referenceFrame; },
+                'set': (val) => { this.referenceFrame = val; },
+                'local': true
+            },
+            'lPos': {
+                'get': () => {
+                    return this.localPos;
+                },
+                'set': undefined
+            }
+            'lRot': {
+                'get': () => {
+                    return this.localRot;
+                },
+                'set': undefined
+            }
+        } );
     }
 }
