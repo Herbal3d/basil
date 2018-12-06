742b4470cfcd07250cbca187f788e99d1bf5c67b
Robert Adams <misterblue@misterblue.com>
2018-12-06 13:57:56 -0800

    Update updateLibs.sh to have conditional sections. Remove some unused copies (old JustView).

--- Basiljs/updateLibs.sh
+++ Basiljs/updateLibs.sh
@@ -7,62 +7,76 @@ cd "$( cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd )"
 PLACE=$(pwd)
 
 JSLIBS=${PLACE}/src/jslibs
-JUSTVIEW=${PLACE}/justview
 
 UPDATEDATE=$(date +%Y%m%d)
 echo "${UPDATEDATE}" > "${JSLIBS}/update.date"
 
-echo "=== Fetch ThreeJS"
+DOUPDATETHREEJS="yes"
+DOUPDATEBASILPROTO="yes"
+DOUPDATEPROTOBUF="yes"
+DOBUILDBASILPROTO="yes"
+
 THREEDIR=$PLACE/../../three.js
-cd "$THREEDIR"
-git checkout master
-git pull --all
-git checkout dev
-git pull
-cp "$THREEDIR/build/three.min.js" "${JSLIBS}"
-cp "$THREEDIR/examples/js/loaders/GLTFLoader.js" "${JSLIBS}"
-cp "$THREEDIR/examples/js/loaders/DRACOLoader.js" "${JSLIBS}"
-cp "$THREEDIR/examples/js/loaders/FBXLoader.js" "${JSLIBS}"
-cp "$THREEDIR/examples/js/loaders/ColladaLoader.js" "${JSLIBS}"
-cp "$THREEDIR/examples/js/loaders/OBJLoader.js" "${JSLIBS}"
-cp "$THREEDIR/examples/js/loaders/BVHLoader.js" "${JSLIBS}"
-cp "$THREEDIR/examples/js/controls/OrbitControls.js" "${JSLIBS}"
-cp "$THREEDIR/examples/js/Detector.js" "${JSLIBS}"
-cp "$THREEDIR/examples/js/libs/stats.min.js" "${JSLIBS}"
+if [[ "$DOUPDATETHREEJS" == "yes" ]] ; then
+    echo "=== Fetch ThreeJS"
+    cd "$THREEDIR"
+    git checkout master
+    git pull --all
+    git checkout dev
+    git pull
+    cp "$THREEDIR/build/three.min.js" "${JSLIBS}"
+    cp "$THREEDIR/examples/js/loaders/GLTFLoader.js" "${JSLIBS}"
+    cp "$THREEDIR/examples/js/loaders/DRACOLoader.js" "${JSLIBS}"
+    cp "$THREEDIR/examples/js/loaders/FBXLoader.js" "${JSLIBS}"
+    cp "$THREEDIR/examples/js/loaders/ColladaLoader.js" "${JSLIBS}"
+    cp "$THREEDIR/examples/js/loaders/OBJLoader.js" "${JSLIBS}"
+    cp "$THREEDIR/examples/js/loaders/BVHLoader.js" "${JSLIBS}"
+    cp "$THREEDIR/examples/js/controls/OrbitControls.js" "${JSLIBS}"
+    cp "$THREEDIR/examples/js/libs/stats.min.js" "${JSLIBS}"
+else
+    echo "=== Not updating ThreeJS"
+fi
 
-cp "$THREEDIR/build/three.min.js" "${JUSTVIEW}"
-cp "$THREEDIR/examples/js/controls/OrbitControls.js" "${JUSTVIEW}"
-cp "$THREEDIR/examples/js/loaders/GLTFLoader.js" "${JUSTVIEW}"
-cp "$THREEDIR/examples/js/Detector.js" "${JUSTVIEW}"
-cp "$THREEDIR/examples/js/libs/stats.min.js" "${JUSTVIEW}"
+BASILPROTODIR=$PLACE/../../Basil-protocol
+if [[ "$DOUPDATEBASILPROTO" == "yes" ]] ; then
+    echo "=== Fetch Basil-protocol"
+    cd "$BASILPROTODIR"
+    git checkout master
+    git pull --all
+else
+    echo "=== Not updating Basil-protocol"
+fi
 
-echo "=== Fetch Basil-protocol"
-BASILPROTDIR=$PLACE/../../Basil-protocol
-cd "$BASILPROTDIR"
-git checkout master
-git pull --all
-
-echo "=== Fetch protobuf.js"
 PROTOBUFJSDIR=$PLACE/../../protobuf.js
-cd "${PROTOBUFJSDIR}"
-chmod -x bin/*
-git checkout master
-git pull --all
-# npm update
-chmod +x bin/*
-mkdir -p "$JSLIBS/protobufjs/minimal"
-cp -r dist/protobuf*.js "$JSLIBS/protobufjs"
-cp -r dist/minimal/protobuf*.js "$JSLIBS/protobufjs/minimal"
+if [[ "$DOUPDATEPROTOBUF" == "yes" ]] ; then
+    echo "=== Updating protobuf.js"
+    cd "${PROTOBUFJSDIR}"
+    chmod -x bin/*
+    git checkout master
+    git pull --all
+    # npm update
+    chmod +x bin/*
+    mkdir -p "$JSLIBS/protobufjs/minimal"
+    cp -r dist/protobuf*.js "$JSLIBS/protobufjs"
+    cp -r dist/minimal/protobuf*.js "$JSLIBS/protobufjs/minimal"
+else
+    echo "=== Not updating ProtoBuf"
+fi
 
-echo "=== Build JS code for Basil-protocol"
-cd "${PROTOBUFJSDIR}"
-./bin/pbjs -t static-module \
+if [[ "$DOBUILDBASILPROTO" == "yes" ]] ; then
+    echo "=== Build JS code for Basil-protocol"
+    cd "${PROTOBUFJSDIR}"
+    ./bin/pbjs -t static-module \
         --wrap es6 \
         --no-comments \
-        $BASILPROTDIR/protocol/BasilServer.proto \
-        $BASILPROTDIR/protocol/BasilTypes.proto \
-        $BASILPROTDIR/protocol/BTransport.proto \
+        $BASILPROTODIR/protocol/BasilServer.proto \
+        $BASILPROTODIR/protocol/BasilTypes.proto \
+        $BASILPROTODIR/protocol/BTransport.proto \
         > "$JSLIBS/BasilServerMessages.js"
+
+else
+    echo "=== Not building JS code for Basil-protocol"
+fi
 
 cd "${PLACE}"
 
