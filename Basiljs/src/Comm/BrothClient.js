b245629b4fd6a7a5f116f24589ee58d2bf6d783c
Robert Adams <misterblue@misterblue.com>
2019-04-01 12:49:20 -0700

    The Broth service in the browser Basil service is a dumb idea because the communication between Basil and SpaceServers should be simple. This adds the code before it is deleted so it is in the git data somewhere.

--- Basiljs/src/Comm/BrothClient.js
+++ Basiljs/src/Comm/BrothClient.js
@@ -17,6 +17,8 @@ import Config from '../config.js';
 import { MsgProcessor } from './MsgProcessor.js';
 import { BasilMessageOps } from './BasilMessageOps.js';
 
+import { Eventing } from '../Eventing/Eventing.js';
+
 import { CreateUniqueId, CombineParameters } from '../Utilities.js';
 
 // Interface for a client talking to a SpaceServer over the Basil/SpaceServer stream
@@ -40,6 +42,20 @@ export class BrothClientConnection extends MsgProcessor {
         processors.set(BasilMessageOps.get('SubscriptionMadeResp'), this._procSubscriptionMade.bind(this));
         processors.set(BasilMessageOps.get('SubscriptionClearResp'), this._procSubscriptionClear.bind(this));
         this.RegisterMsgProcess(this.transport, processors);
+
+        // Connect to the eventing system.
+        // Register all existing topics so remote people can subscribe
+        this.events = Eventing.Instance();
+        let auth = undefined;
+        this.events.RegisteredTopicEntries().forEach( topicEntry => {
+            this.RegisterTopic(auth, topicEntry.topic, undefined);
+        });
+        this.events.RegisterEvent(_procRegister);
+        this.events.UnRegisterEvent(_procUnregister);
+        this.events.SubscriptionEvent(_procSubscription);
+        this.events.FireEvent(_procFire);
+
+        this.remoteSubscriptions = new Map();
     };
 
     Start() {
@@ -47,7 +63,6 @@ export class BrothClientConnection extends MsgProcessor {
 
     Close() {
     };
-
     RegisterTopic(auth, topic, propertyList) {
         let msg = { 'op': BasilMessageOps.get('RegisterTopicReq') };
         if (auth) msg['auth'] = auth;
@@ -58,44 +73,87 @@ export class BrothClientConnection extends MsgProcessor {
     DeregisterTopic(auth, topic) {
         let msg = { 'op': BasilMessageOps.get('DeregisterTopicReq') };
         if (auth) msg['auth'] = auth;
-        if (reason) msg['reason'] = reason;
+        let props = { 'topic': topic };
+        if (reason) props['reason'] = reason;
+        msg['properties'] = props;
         return this.SendAndPromiseResponse(msg);
     };
     Subscribe(auth, topic, propertyList) {
         let msg = { 'op': BasilMessageOps.get('SubscribeReq') };
         if (auth) msg['auth'] = auth;
-        msg['topic'] = topic;
-        if (propertyList) msg['properties'] = this.CreatePropertyList(propertyList);
+        let props = { 'topic': topic }
+        if (propertyList) Object.assign(props, this.CreatePropertyList(propertyList));
+        msg['properties'] = props;
         return this.SendAndPromiseResponse(msg);
     }
     Unsubscribe(auth, topic) {
         let msg = { 'op': BasilMessageOps.get('UnsubscribeReq') };
         if (auth) msg['auth'] = auth;
-        msg['topic'] = topic;
+        let props = { 'topic': topic }
+        msg['properties'] = props;
         return this.SendAndPromiseResponse(msg);
     }
     SendEvent(auth, topic, featureList) {
         let msg = { 'op': BasilMessageOps.get('SendEventReq') };
         if (auth) msg['auth'] = auth;
-        msg['topic'] = topic;
-        if (featureList) msg['features'] = this.CreatePropertyList(featureList);
+        let props = { 'topic': topic }
+        if (featureList) Object.assign(props, this.CreatePropertyList(featureList));
+        msg['properties'] = props;
         return this.SendAndPromiseResponse(msg);
     }
     SendEventNoWait(auth, topic, featureList) {
         let msg = { 'op': BasilMessageOps.get('SendEventReq') };
         if (auth) msg['auth'] = auth;
-        msg['topic'] = topic;
-        if (featureList) msg['features'] = this.CreatePropertyList(featureList);
+        let props = { 'topic': topic }
+        if (featureList) Object.assign(props, this.CreatePropertyList(featureList));
+        msg['properties'] = props;
         this.SendMessage(msg);
     }
 
     _procEventReceived(responseMsg) {
-
+        if (responseMsg.properties) {
+            let topic = responseMsg.properties['topic'];
+            if (topic) {
+                this.events.Fire(topic, responseMsg.properties);
+            }
+        }
     }
     _procSubscriptionMade(responseMsg) {
-
+        if (responseMsg.properties) {
+            let topic = responseMsg.properties['topic'];
+            if (topic) {
+                if (!this.remoteSubscriptions.has(topic)) {
+                    this.remoteSubscriptions.set(topic, '');
+                }
+            }
+        }
     }
     _procSubscriptionClear(responseMsg) {
-
+        if (responseMsg.properties) {
+            let topic = responseMsg.properties['topic'];
+            if (topic) {
+                this.remoteSubscriptions.delete(topic);
+            }
+        }
     }
+
+    // Someone local registered a topic. Tell the remote system.
+    _procRegister(topic) {
+        this.RegisterTopic(undefined, topic, undefined)
+    }
+    // Someone local unregistered a topic. Tell the remote system.
+    _procUnregister(topic) {
+        this.DeregisterTopic(undefined, topic);
+    }
+    // Someone local subscribed to a topic. Tell the remote system.
+    _procSubscription(topic) {
+        this.Subscribe(undefined, topic, undefined);
+    }
+    // Someone local fired an event. Tell the remote system.
+    _procFire(topic, params) {
+        if (this.remoteSubscriptions.has(topic)) {
+            this.SendEventNoWait(undefined, topic, params);
+        }
+    }
+
 }
