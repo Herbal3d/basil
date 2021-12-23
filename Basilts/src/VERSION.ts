bca2cc24869bb3acc3cb8e32b8c29f5cdc5ac527
Robert Adams <misterblue@misterblue.com>
2021-12-23 15:46:50 +0000

    Update NPM package versions. Bump version to 2.1.3

--- Basilts/src/VERSION.ts
+++ Basilts/src/VERSION.ts
@@ -1,5 +1,5 @@
 export const VERSION = {
     "npm-package-version": "2.1.2",
-    "git-commit": "3d90848dec31a55417ec55eb06f3f76b37dc9fb5",
-    "version-tag": "2.1.2-20211102-3d90848"
+    "git-commit": "c5b2089d3ab34b96acc25476d50c6b9a734bbb51",
+    "version-tag": "2.1.2-20211223-c5b2089"
 }
